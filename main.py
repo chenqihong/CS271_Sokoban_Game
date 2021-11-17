bd2162135e55b68ce95484c9115d4a190399824e
Dheyay <desaidn@uci.edu>
2021-11-17 15:11:40 -0800

    BFS pathfinding function complete

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ __copyright__ = "Copyright 2021, The game of Sokoban game project"
 from constant_configuration import *
 from base_implementation import *
 from game_board_class import *
+from pathfinding import *
 
 
 def test_game(my_game_board: GameBoard) -> None:
@@ -19,12 +20,10 @@ def test_game(my_game_board: GameBoard) -> None:
 
 if __name__ == '__main__':
     input_str = "sokoban01.txt"
-    my_game_board_object = read_input(input_str)
-    my_game_board_object.show_board()
-
-    #update_corner_state_value_table(my_game_board_object)
-    #print("state_value_table = ", dict(state_value_table))
-    #update_between_state_value_table(my_game_board_object)
-    #for key in state_value_table:
-    #    print("coordinate: ", key, " state value = ", state_value_table[key])
+    board = read_input(input_str)
+    update_corner_state_value_table(board)
+    print("state_value_table = ", dict(state_value_table))
+    update_between_state_value_table(board)
+    for key in state_value_table:
+        print("coordinate: ", key, " state value = ", state_value_table[key])
     # test_game(my_game_board_object)
--- pathfinding.py
+++ pathfinding.py
@@ -27,44 +27,58 @@ def is_out_of_bounds(board_size, position):
     return False
 
 
-def get_neighbors(board:GameBoard, position):
+def get_neighbors(board: GameBoard, position, goal):
     """
     :param board: current board state
     :param position: coordinate on grid
     :return: neighbors: a list of neighboring coordinates for given position
     """
-    possible_neighbors = [(position[0]+1, position[1]), (position[0]-1, position[1]),
-                 (position[0], position[1]+1), (position[0], position[1]-1)]
+    possible_neighbors = [(position[0] + 1, position[1]), (position[0] - 1, position[1]),
+                          (position[0], position[1] + 1), (position[0], position[1] - 1)]
     valid_neighbors = []
+
+    box = board.box_coordinate_list.copy()
+    box.remove(goal)
 
     # ignore walls when getting neighbors because cannot move to wall
     # make sure found neighbor is inside the bounds of the game board
     for neighbor in possible_neighbors:
-        if neighbor not in board.wall_coordinate_list and \
+        if neighbor not in board.wall_coordinate_list and neighbor not in box and \
                 not is_out_of_bounds(board_size=board.get_dimension(), position=neighbor):
             valid_neighbors.append(neighbor)
 
     return valid_neighbors
 
 
-def get_accessible_boxes(board:GameBoard):
+def get_accessible_boxes(board: GameBoard):
     # Do BFS path search on each box location
     # If there is no path for box then it cannot be reached
     # Not considering paths that include moving boxes (edge case)
     pass
 
 
-
 def bfs(board: GameBoard, box_location):
     """
+    Returns all possible paths to location from player position
+    :param box_location: Location of box to be reached
     :param board: Current game board state
-    :return:
+    :return: all possible paths avoiding walls and other boxes
+                to box location
     """
     # Use Nodes to trace path
-    pass
+    final_paths = []
+    start_pos = board.get_current_player_coordinate()
+    queue = [[start_pos]]
+    while queue:
+        cur_path = queue.pop()
+        cur_pos = cur_path[-1]
 
-
-
-
-
-
+        if cur_pos == box_location:
+            final_paths.append(cur_path)
+            continue
+        for neighbor in get_neighbors(board, cur_pos, box_location):
+            if neighbor not in cur_path:
+                new_path = list(cur_path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+    return final_paths
